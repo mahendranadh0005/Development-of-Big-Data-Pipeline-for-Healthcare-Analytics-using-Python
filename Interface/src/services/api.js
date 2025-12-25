c6ba36f02ff803bc9cb90a997c478c55fff091ba
mahendranadh0005 <jujapumahendranadh@gmail.com>
2025-12-25 23:51:20 +0530

    Update API service configuration

--- Interface/src/services/api.js
+++ Interface/src/services/api.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true, // safe for auth
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Attach token automatically
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
