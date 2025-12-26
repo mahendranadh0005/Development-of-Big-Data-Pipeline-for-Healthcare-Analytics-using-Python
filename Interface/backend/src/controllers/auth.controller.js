756bc46b09c09a2d5c9a8663d1a432c864cc40b0
mahendranadh0005 <jujapumahendranadh@gmail.com>
2025-12-26 23:44:23 +0530

    authhhh

--- Interface/backend/src/controllers/auth.controller.js
+++ Interface/backend/src/controllers/auth.controller.js
@@ -42,3 +42,21 @@ export const login = async (req, res) => {
   console.log("bcrypt match:", await bcrypt.compare(password, user.password));
 
 };
+
+
+export const protect = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Not authorized, no token" });
+  }
+
+  try {
+    const token = authHeader.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Not authorized, token failed" });
+  }
+};
