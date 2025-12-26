b756921b23ae718cccbb7d43464c8af7eff49393
mahendranadh0005 <jujapumahendranadh@gmail.com>
2025-12-26 21:06:47 +0530

    consolemessage

--- Interface/backend/src/controllers/auth.controller.js
+++ Interface/backend/src/controllers/auth.controller.js
@@ -9,7 +9,7 @@ export const login = async (req, res) => {
     const user = await User.findOne({ username });
 
     if (!user) {
-      return res.status(401).json({ message: "Invalid credentials" });
+      return res.status(401).json({ message: "Invalid username" });
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
