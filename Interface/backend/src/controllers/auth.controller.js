824298b8e9df856ca4c981d8ebcbd6239afecb69
mahendranadh0005 <jujapumahendranadh@gmail.com>
2025-12-26 21:03:58 +0530

    consolemsg

--- Interface/backend/src/controllers/auth.controller.js
+++ Interface/backend/src/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(401).json({ message: "Invalid credentials" });
+      return res.status(401).json({ message: "Invalid Password" });
     }
 
     const token = jwt.sign(
