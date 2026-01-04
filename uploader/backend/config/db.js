9e8cddee0eb7d63d916daaffe9b6310f9cfab010
mahendranadh0005 <jujapumahendranadh@gmail.com>
2026-01-04 22:34:51 +0530

    dummy first comit

--- uploader/backend/config/db.js
+++ uploader/backend/config/db.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "test"
+  });
+
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error);
